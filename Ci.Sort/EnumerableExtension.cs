c478e685df101e8877a4000d5052a89526fe5588
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2018-03-21 09:24:08 +0100

    temporary solution(need code refactoring)

--- Ci.Sort/EnumerableExtension.cs
+++ Ci.Sort/EnumerableExtension.cs
@@ -1,10 +1,8 @@
 ﻿using Ci.Sort.Enums;
 using Ci.Sort.Models;
-using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Reflection;
 
 namespace Ci.Sort
 {
@@ -12,35 +10,6 @@ namespace Ci.Sort
     {
         public static IEnumerable<T> Sort<T>(this IEnumerable<T> query, SortOrder sort)
         {
-            PropertyDescriptor GetKeyProperty()
-            {
-                var properties = GetAllProperties(typeof(T));
-
-                var keyProp = new PropertyDescriptorCollection(properties.ToArray())
-                    .Find(sort.Key, true);
-                return keyProp;
-            }
-
-            List<PropertyDescriptor> GetAllProperties(Type baseType)
-            {
-                var resList = new List<PropertyDescriptor>();
-                foreach (PropertyDescriptor propertyDescriptor in TypeDescriptor.GetProperties(baseType))
-                {
-                    resList.Add(propertyDescriptor);
-                    var declaredProperties = propertyDescriptor.ComponentType.GetTypeInfo().DeclaredProperties.ToList();
-                    if (propertyDescriptor.Attributes.Count == 0 && declaredProperties.Count != 0)
-                    {
-                        foreach (var propertyInfo in declaredProperties)
-                        {
-                            var fields = ((TypeInfo)propertyInfo.PropertyType).DeclaredFields;
-                            resList.AddRange(PropertyDescriptor(fields.First()).Cast<PropertyDescriptor>());
-                        }
-                    }
-                }
-
-                return resList;
-            }
-
             if (sort == null)
                 sort = new SortOrder();
 
@@ -53,22 +22,43 @@ namespace Ci.Sort
                     .Name;
             }
 
-            var prop = GetKeyProperty();
+            var properties = TypeDescriptor.GetProperties(typeof(T));
 
-            query = sort.Order == Order.Ascending
-                ? query.OrderBy(x => prop.GetValue(x))
-                : query.OrderByDescending(x => prop.GetValue(x));
+            var keyProp = properties
+                .Find(sort.Key, true);
+            PropertyDescriptor baseProp = null;
+
+            if (keyProp == null)
+            {
+                foreach (PropertyDescriptor property in properties)
+                {
+                    baseProp = property;
+                    var childProperties = property.GetChildProperties();
+                    keyProp = childProperties
+                        .Find(sort.Key, true);
+
+                    if (keyProp != null) break;
+                }
+
+                if (keyProp != null)
+                {
+                    query = sort.Order == Order.Ascending
+                        ? query.OrderBy(x => keyProp.GetValue(baseProp.GetValue(x)))
+                        : query.OrderByDescending(x => keyProp.GetValue(baseProp.GetValue(x)));
+                }
+                else
+                {
+                    //todo throw exception????
+                }
+            }
+            else
+            {
+                query = sort.Order == Order.Ascending
+                    ? query.OrderBy(x => keyProp.GetValue(x))
+                    : query.OrderByDescending(x => keyProp.GetValue(x));
+
+            }
             return query;
-        }
-
-        public static PropertyDescriptor PropertyDescriptor(this PropertyInfo propertyInfo)
-        {
-            return TypeDescriptor.GetProperties(propertyInfo.DeclaringType)[propertyInfo.Name];
-        }
-
-        public static PropertyDescriptorCollection PropertyDescriptor(this FieldInfo fieldInfo)
-        {
-            return TypeDescriptor.GetProperties(fieldInfo.DeclaringType);
         }
     }
 }
